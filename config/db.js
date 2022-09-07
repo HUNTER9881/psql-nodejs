01d5ea5042a4cf768adb6a9173574a28bde2e037
gargamelNodejs <shahriyornodejs@mail.ru>
2022-09-07 20:35:17 +0500

    asd

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 const poll = new Pool({
     user: "postgress",
     password:  "12345",
-    database: "employer",
+    database: "test",
     host:  "localhost",
     port: "5432"
 })
