695c7d2e6a6dff172918addeb7f0a525375f7349
gargamelNodejs <shahriyornodejs@mail.ru>
2022-09-07 16:09:09 +0500

    asd

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const Pool = require('pg').Pool;
 const poll = new Pool({
     user: "postgres",
     password:  "12345",
-    database: "postgres",
+    database: "employer",
     host:  "localhost",
-    port: "5432" 
+    port: "5432"
 })
 
 module.exports = poll
