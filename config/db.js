c78606b17b307d3833b7287a32a43bac177665e7
gargamelNodejs <shahriyornodejs@mail.ru>
2022-09-07 20:44:36 +0500

    asd

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 const poll = new Pool({
     user: "dilshod",
-    password:  "12345",
+    password:  "1585743d",
     database: "ecom",
     host:  "localhost",
     port: "5432"
