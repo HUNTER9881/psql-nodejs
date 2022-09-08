0249c3da78494d483cda0d0c17e10159a468a95a
gargamelNodejs <shahriyornodejs@mail.ru>
2022-09-09 02:28:10 +0500

    asd

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const pg = require('pg');
-const conString = "postgres://postgres:12345@localhost:5432/employer";
+// const conString = "postgres://postgres:12345@localhost:5432/employer";
+const conString = 'postgres://admin:Passw0rd@192.168.205.10:5432/postgres?sslmode=disable';
 const client = new pg.Client(conString);
 module.exports = client.connect().then(() => {
     console.log("PostgreSQL is connected")
